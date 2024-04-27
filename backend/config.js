e2b24950cf458b80d72a4c90dead587de008afe7
Mark <mrkaxelus@gmail.com>
2024-04-27 01:27:09 -0700

    express setup

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT || 3000;
+/* Using process.env.PORT for flexibility purposes, it ensures that the application can run on different environments without
+changes to the code. Environment variables are often used for various deployment and production environments which can be
+set by the OS, User, or the Cloud environemnt (hosting platform) */
