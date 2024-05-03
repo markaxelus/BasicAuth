5333abb6e4e3e06424e8fce72dc17d5f736c8e29
Mark <mrkaxelus@gmail.com>
2024-05-03 08:35:49 -0600

    Navbar pre-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+// App.jsx
 import React from 'react'
+import { Routes, Route } from 'react-router-dom' 
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes> 
+    </>
   )
 }
 
